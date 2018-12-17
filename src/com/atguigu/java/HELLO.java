4ff5225bba1e19bbbff8945af567974a17c1c1d0
4yuemaocaowu <hjhsc307308@163.com>
2018-12-17 21:14:10 +0800

    Initial commit

---
+++ src/com/atguigu/java/HELLO.java
@@ -0,0 +1,8 @@
+package com.atguigu.java;
+
+/**
+ * @author HJ
+ * @date 2018/12/16-15:53
+ */
+public class HELLO {
+}
---
+++ src/com/atguigu/java/helloworld.java
@@ -0,0 +1,14 @@
+package com.atguigu.java;
+
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld!!");
+        String[] arr=new String[]{"LILEI","HANMEIMEI"};
+            for (String s : arr) {
+                System.out.println(s);
+
+            }
+
+
+    }
+}
